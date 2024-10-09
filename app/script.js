7d7c121c04f5f494cb1bc2a3b7d12edb20110a48
xabrauu <130366440+xabrauu@users.noreply.github.com>
2024-10-09 08:26:48 -0300

    Add files via upload

---
+++ app/script.js
@@ -0,0 +1,4 @@
+document.getElementById('saudacaoBtn').addEventListener('click', function() {
+    const nome = document.getElementById('nome').value;
+    const mensagem = document.getElementById('mensagem');
+});
