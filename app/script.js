af2c04324a39929136eb5813737656755c960dec
xabrauu <130366440+xabrauu@users.noreply.github.com>
2024-10-31 20:25:09 -0300

    Delete app directory

--- app/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('saudacaoBtn').addEventListener('click', function() {
-    const nome = document.getElementById('nome').value;
-    const mensagem = document.getElementById('mensagem');
-});
